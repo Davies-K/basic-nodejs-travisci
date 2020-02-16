41cfb6068e036b10974bb8c34121954fd4de69f7
Davies123 <john.daves13@gmail.com>
2020-02-16 15:16:13 +0100

    added comments

--- index.js
+++ index.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const app = express();
 
+
+// send get request 
 app.get('/', (req, res) => {
     res.send('ci with travis')
 })
 
+// Port the express app will run on
 const server = app.listen(3000, () => {
     console.log('running on port 3000')
 })
